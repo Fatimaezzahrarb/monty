11ccb62d185b3263ba002a63d6a029b2ba3b48bd
Fatimaezzahrarb <north.winds04@gmail.com>
2023-09-21 12:38:43 -0700

    Python

---
+++ queue.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * f_queue - prints in the top.
+ * @head: stack head
+ * @counter: line.number
+ * Return: NO return
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add a node to the end of the stack.
+ * @n: new.value
+ * @head: head of The stack
+ * Return: NO return
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints in the top.
+ * @head: stack head
+ * @counter: line.number
+ * Return: NO return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
