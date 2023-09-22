31c394f940825218dfc0a26fc78f6c09109f7e54
Fatimaezzahrarb <north.winds04@gmail.com>
2023-09-22 11:51:39 -0700

    python

--- monty.h
+++ monty.h
@@ -7,15 +7,14 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
-#define  _POSIX_C_SOURCE 200809L
 /**
- * struct stack_s - doubly linked list Representation of A stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO HolBerton project.
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -26,10 +25,10 @@ typedef struct stack_s
 /**
  * struct bus_s - variables -args, file, line content
  * @arg: value
- * @file: pointer To  monty file
+ * @file: pointer to monty file
  * @content: line content
- * @lifi: flag Change stack <-> queue
- * Description: carries values through THE program.
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
  */
 typedef struct bus_s
 {
@@ -40,11 +39,11 @@ typedef struct bus_s
 }  bus_t;
 extern bus_t bus;
 /**
- * struct instruction_s - opcode and ITS function.
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
- * @f: function to handle THE opcode
+ * @f: function to handle the opcode
  *
- * Description: opcode and ITS function
+ * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
